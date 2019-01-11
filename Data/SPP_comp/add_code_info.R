a1911abe6db76d7c13b10f891a91de5571359c58
SmithEcophysLab <nick.gregory.smith@gmail.com>
2019-01-11 14:29:45 -0600

    added 2018 data that Risa collated

---
+++ Data/SPP_comp/add_code_info.R
@@ -0,0 +1,19 @@
+sc = read.csv('/Users/nicksmith/Documents/Git/NutNet_LBK/Data/SPP_comp/species_comp.csv')
+sp = read.csv('/Users/nicksmith/Documents/Git/NutNet_LBK/Data/SPP_comp/species.csv')
+
+sc$binomial = 'NA'
+sc$lifeform = 'NA'
+sc$family = 'NA'
+
+for (i in 1:length(sc$binomial)){
+	
+	sc$binomial[i] <- as.character(subset(sp, Code == as.character(sc$Code[i]))$Latin.binomial)
+	sc$lifeform[i] <- as.character(subset(sp, Code == as.character(sc$Code[i]))$Life.form)
+	sc$family[i] <- as.character(subset(sp, Code == as.character(sc$Code[i]))$Family)
+	
+}
+
+sc$genus = vapply(strsplit(sc$binomial, " ", fixed = TRUE), "[", "", 1)
+sc$species = vapply(strsplit(sc$binomial, " ", fixed = TRUE), "[", "", 2)
+
+write.csv(sc, '/Users/nicksmith/Documents/Git/NutNet_LBK/Data/SPP_comp/species_comp_withinfo.csv')
