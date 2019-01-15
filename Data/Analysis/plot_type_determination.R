519216323c473c3b7bfce9f79e29a8d514cdddff
SmithEcophysLab <nick.gregory.smith@gmail.com>
2019-01-15 13:00:18 -0600

    revised plot type determination to include fenced plots

--- Data/Analysis/plot_type_determination.R
+++ Data/Analysis/plot_type_determination.R
@@ -18,7 +18,7 @@ library(factoextra) # clustering algorithms & visualization
 #######################################
 biomass = read.csv('../Biomass/Biomass.csv')
 light = read.csv('../Light/Light.csv')
-comp = read.csv('../SPP_comp/species_comp_withinfo.csv')
+comp = read.csv('../SPP_comp/species_comp.csv')
 
 #######################################
 # subset to only include September 2018 data
@@ -52,8 +52,8 @@ plots = seq(1, 42, 1)
 light_avail_data = NULL
 for (i in 1:length(plots)){
 	
-	in_mean = mean(subset(light_18, Plot == i & Position == 'In')$Light)
-	out_mean = subset(light_18, Plot == i & Position == 'Out')$Light
+	in_mean = mean(subset(light_18, Plot == i & Location == 'Ground')$Light)
+	out_mean = subset(light_18, Plot == i & Location == 'Ambient')$Light
 	avail = in_mean / out_mean
 	plot = i
 	light_avail_data = rbind(c(plot, avail), light_avail_data)
@@ -87,28 +87,28 @@ rownames(dlb_matrix) = seq(1, 42, 1)
 
 # find plots with greatest average Euclidean distance
 distances = colMeans(as.matrix(dist(scale(dlb_matrix), method = 'euclidean', diag = T, upper = T)))
-sort(distances[1:14], decreasing = T) # plots 1, 7, 6, 10, 11, 2
-sort(distances[15:28], decreasing = T) # plots 25, 16, 22, 19, 26, 20
-sort(distances[29:42], decreasing = T) # plots 40, 31, 38, 41, 42, 32
+sort(distances[1:14], decreasing = T) # plots 1, 6, 7, 10
+sort(distances[15:28], decreasing = T) # plots 25, 22, 19, 26
+sort(distances[29:42], decreasing = T) # plots 31, 40, 38, 41
 
 # some k-means plots
 fviz_nbclust(scale(dlb_matrix), kmeans, method = "wss") # 4
-k_dlb_block = kmeans(scale(dlb_matrix), centers = 9, nstart = 25)
+k_dlb_block = kmeans(scale(dlb_matrix), centers = 7, nstart = 25)
 fviz_cluster(k_dlb_block, data = scale(dlb_matrix)) # 
 
 #######################################
 # assign plot types to similar plots
 #######################################
 
-plot_types = c('Control', 'N', 'P', 'K', 'NP', 'NK', 'PK', 'NPK')
+plot_types = c('Control', 'N', 'P', 'K', 'NP', 'NK', 'PK', 'NPK', 'Fence', 'NPK+Fence')
 
 # plot_types_block1 = sample(plot_types)
 # plot_types_block2 = sample(plot_types)
 # plot_types_block3 = sample(plot_types)
 
-plot_types_block1_all = c('NA', 'NA', plot_types_block1[1:3], 'NA', 'NA', plot_types_block1[4:5], 'NA', 'NA', plot_types_block1[6:8]) # remove 1, 2, 6, 7, 10, 11
-plot_types_block2_all = c(plot_types_block2[1], 'NA', plot_types_block2[2:3], 'NA', 'NA', plot_types_block2[4], 'NA', plot_types_block2[5:6], 'NA', 'NA', plot_types_block2[7:8]) # remove 16, 19, 20, 22, 25, 26
-plot_types_block3_all = c(plot_types_block3[1:2], 'NA', 'NA', plot_types_block3[3:7], 'NA', plot_types_block3[8], 'NA', 'NA', 'NA') # remove 31, 32, 38, 40, 41, 42
+plot_types_block1_all = c('xControl', plot_types_block1[1:4], 'xControl', 'xControl', plot_types_block1[5:6], 'xControl', plot_types_block1[7:10]) # remove 1, 6, 7, 10
+plot_types_block2_all = c(plot_types_block2[1:4], 'xControl', plot_types_block2[5:6], 'xControl', plot_types_block2[7:8], 'xControl', 'xControl', plot_types_block2[9:10]) # remove 19, 22, 25, 26
+plot_types_block3_all = c(plot_types_block3[1:2], 'xControl', plot_types_block3[3:8], 'xControl', plot_types_block3[9], 'xControl', 'xControl', plot_types_block3[10]) # remove 31, 38, 40, 41
 
 plot_type_assignment = as.data.frame(cbind(seq(1, 42, 1), NA))
 colnames(plot_type_assignment) = c('Plot', 'trt')
@@ -121,8 +121,10 @@ nrow(subset(plot_type_assignment, trt == 'NP'))
 nrow(subset(plot_type_assignment, trt == 'NK'))
 nrow(subset(plot_type_assignment, trt == 'PK'))
 nrow(subset(plot_type_assignment, trt == 'NPK'))
+nrow(subset(plot_type_assignment, trt == 'Fence'))
+nrow(subset(plot_type_assignment, trt == 'NPK+Fence'))
 
-write.csv(plot_type_assignment, '../../plot_types/plot_types.csv')
+# write.csv(plot_type_assignment, '../../plot_types/plot_types.csv')
 
 
 
